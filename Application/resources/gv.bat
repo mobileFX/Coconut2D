5597ec764270f257e77f8740307ac2bf274e340c
mobilefx <epolitakis@mobilefx.com>
2016-09-02 01:06:44 +0300

    Updated Presentation

--- Application/resources/gv.bat
+++ Application/resources/gv.bat
@@ -2,4 +2,5 @@ set GRAPHVIZ=D:\mobileFX\Projects\Software\Coconut\IDE\Studio\etc\3rd_party\grap
 set DIAGRAMS=D:\mobileFX\Projects\Software\Coconut\Projects\Coco.project\Application\resources
 set INPUT=D:\mobileFX\Projects\Software\Coconut\Projects\Coco.project\Application\resources\SlotGameEngine.gv
 set OUTPUT=D:\mobileFX\Projects\Software\Coconut\Projects\Coco.project\Application\resources\SlotGameEngine
+"%GRAPHVIZ%\dot.exe" -Tpdf  -o"%OUTPUT%.pdf" "%INPUT%"
 "%GRAPHVIZ%\dot.exe" -n -Tpng  -o"%OUTPUT%.png" "%INPUT%"
